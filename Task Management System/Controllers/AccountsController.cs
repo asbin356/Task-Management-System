6afdc83ae5e70fa20edaba47693eb1681123bfa6
Asbin <asbin356@gmail.com>
2025-02-06 13:14:19 +0545

    Implemented Register and Login methods

--- Task Management System/Controllers/AccountsController.cs
+++ Task Management System/Controllers/AccountsController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Task_Management_System.Services;
+using Task_Management_System.ViewModels.AccountsViewModels;
 
 namespace Task_Management_System.Controllers
 {
+    /// <summary>
+    /// In AccountsController we will be creating a login and register methods
+    /// </summary>
     public class AccountsController : Controller
     {
-        public IActionResult Index()
+        private IUserService _userService;
+
+        public AccountsController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.RegisterUserAsync(model);
+                return RedirectToAction("Login", "Accounts");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userService.AuthenticateUserAsync
+                    (model.Username, model.Password);
+                if (user != null)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name,user.Username),
+                        new Claim(ClaimTypes.Email,user.Email)
+                    };
+
+                    var claimIdentity = new ClaimsIdentity
+                        (claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties();
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimIdentity), authProperties);
+                }
+                //if the user is null
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempts");
+            }
+            return View();
+        }
+
     }
 }
