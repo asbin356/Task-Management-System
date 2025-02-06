df0c87ab9537aaa0362ef156376b8954360b1857
Asbin <asbin356@gmail.com>
2025-02-06 12:12:41 +0545

    ViewModels for Accounts

---
+++ Task Management System/ViewModels/AccountsViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Task_Management_System.ViewModels.AccountsViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
